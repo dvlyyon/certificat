d35388481d5140dd1205a901f12670d19cbd1b5c
nobody <noreply@git.com>
2022-08-19 13:53:13 +0800

    update

---
+++ gen_cert_rsa_diff.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+crt_dir=.
+pr_dir=./private
+db_dir=./db
+ncrt_dir=./certs
+
+ca_name="ca"
+leaf_name3072="myne1_3072"
+leaf_name4096="myne1_4096"
+
+#rm -rf $pr_dir
+#rm -rf $db_dir
+#rm -rf $ncrt_dir
+#rm -rf $crt_dir/*.crt
+#rm -rf $crt_dir/*.csr
+
+#mkdir -p $pr_dir
+#mkdir -p $db_dir
+#mkdir -p $ncrt_dir
+
+#echo 10 > $db_dir/$ca_name.srl
+#touch $db_dir/$ca_name.index
+
+openssl genrsa -out $pr_dir/$leaf_name3072.key 3072
+openssl pkcs8 -topk8 -nocrypt -in $pr_dir/$leaf_name3072.key -out $pr_dir/$leaf_name3072.pkcs8.key
+openssl req -new -sha384 -key $pr_dir/$leaf_name3072.pkcs8.key -config $leaf_name3072.cnf -out $crt_dir/$leaf_name3072.csr
+
+openssl genrsa -out $pr_dir/$leaf_name4096.key 4096
+openssl pkcs8 -topk8 -nocrypt -in $pr_dir/$leaf_name4096.key -out $pr_dir/$leaf_name4096.pkcs8.key
+openssl req -new -sha512 -key $pr_dir/$leaf_name4096.pkcs8.key -config $leaf_name4096.cnf -out $crt_dir/$leaf_name4096.csr
+
+openssl ca -config $ca_name.cnf -in $crt_dir/$leaf_name3072.csr -out $crt_dir/$leaf_name3072.crt
+openssl ca -config $ca_name.cnf -in $crt_dir/$leaf_name4096.csr -out $crt_dir/$leaf_name4096.crt
+
+
+openssl pkcs12 -export -name "My Certificates $leaf_name4096" -out $leaf_name4096.pfx -inkey private/$leaf_name4096.key -in $leaf_name4096.crt 
+openssl pkcs12 -export -name "My Certificates $leaf_name3072" -out $leaf_name3072.pfx -inkey private/$leaf_name3072.key -in $leaf_name3072.crt 
+
+
