a7e4f1b516c35d359676a905cfbc0ae33cb7635c
nobody <noreply@git.com>
2022-10-14 15:18:58 +0800

    update

--- gen_cert_ecdsa_one_384.sh
+++ gen_cert_ecdsa_one_384.sh
@@ -4,13 +4,13 @@ pr_dir=./ecdsa_private
 db_dir=./ecdsa_db
 ncrt_dir=./ecdsa_certs
 
-ca_name="ecdsa_ca"
+ca_name="ecdsa_ca11"
 #leaf_name="ecdsa_myne1_ipv6"
-leaf_name="ecdsa_myne1_521_nohost"
+leaf_name="ecdsa_myne111_384"
 
 
 
-openssl ecparam -name prime256v1 -genkey -out $pr_dir/$leaf_name.key
+openssl ecparam -name secp384r1 -genkey -out $pr_dir/$leaf_name.key
 openssl pkcs8 -topk8 -nocrypt -in $pr_dir/$leaf_name.key -out $pr_dir/$leaf_name.pkcs8.key
 openssl req -new -sha512 -key $pr_dir/$leaf_name.pkcs8.key -config $leaf_name.cnf -out $crt_dir/$leaf_name.csr
 
